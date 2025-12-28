4a7cc9ac3a7b943cd84fb0453061659b7aa50656
faisal-din <dahrifaisal56@gmail.com>
2025-12-28 22:29:16 +0500

    Landing page created

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Banner from '../Components/Home/Banner';
+import Hero from '../Components/Home/Hero';
+import Features from '../Components/Home/Features';
+import Testimonial from '../Components/Home/Testimonial';
+import CallToAction from '../Components/Home/CallToAction';
+import Footer from '../Components/Home/Footer';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Banner />
+      <Hero />
+      <Features />
+      <Testimonial />
+      <CallToAction />
+      <Footer />
+    </div>
+  );
 };
 
 export default HomePage;
