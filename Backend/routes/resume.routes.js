b5a6b8b5efacd86946ef231175144ea47bd7ac3e
faisal-din <dahrifaisal56@gmail.com>
2026-01-08 21:36:38 +0500

    fixed issue in resume preview page, now you can fetch real time data

--- Backend/routes/resume.routes.js
+++ Backend/routes/resume.routes.js
@@ -15,6 +15,6 @@ resumeRouter.post('/create', protect, createResume);
 resumeRouter.put('/update', upload.single('image'), protect, updateResume);
 resumeRouter.delete('/delete/:resumeId', protect, deleteResume);
 resumeRouter.get('/get/:resumeId', protect, getResumeById);
-resumeRouter.get('/public/:resumeId', protect, getPublicResumeById);
+resumeRouter.get('/public/:resumeId', getPublicResumeById);
 
 export default resumeRouter;
--- Frontend/src/Pages/PreviewPage.jsx
+++ Frontend/src/Pages/PreviewPage.jsx
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { dummyResumeData } from '../assets/assets';
 import ResumePreview from '../Components/ResumePreview';
 import Loader from '../Components/Loader';
 import { ArrowLeftIcon } from 'lucide-react';
+import api from '../configs/api';
+import { useSelector } from 'react-redux';
 
 const PreviewPage = () => {
   const { resumeId } = useParams();
+  const { token } = useSelector((state) => state.auth);
 
   const [isLoading, setIsLoading] = useState(true);
   const [resumeData, setResumeData] = useState(null);
 
   const loadResume = async () => {
-    setResumeData(
-      dummyResumeData.find((resume) => resume._id === resumeId) || null
-    );
-    setIsLoading(false);
+    try {
+      const { data } = await api.get(`/api/resumes/public/${resumeId}`);
+      setResumeData(data.resume);
+    } catch (error) {
+      console.error('Error loading resume:', error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
